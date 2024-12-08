acab7aee19e4cb90636520c46dccd855cfb2fd0c
Alexey <mavbr32@gmail.com>
2024-12-08 22:01:58 +0300

    Первоначальные насмтройки

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,8 +1,37 @@
 package main
 
-import "github.com/julienschmidt/httprouter"
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func IndexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	name := params.ByName("name")
+	w.Write([]byte(fmt.Sprintf("Hello %s", name)))
+
+}
 
 func main() {
 	router := httprouter.New()
+	router.GET("/:name", IndexHandler)
+
+	listener, err := net.Listen("tcp", ":1234")
+
+	if err != nil {
+		panic(err)
+	}
+
+	server := &http.Server{
+		Handler:      router,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatalln(server.Serve(listener))
 
 }
